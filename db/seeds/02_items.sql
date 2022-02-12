73a4c10ffbacf4084eef6b888b9ce50ef454b706
AbdulmajeedS <abdulmajeed.sabriye777@gmai.com>
2022-02-12 19:35:33 +0000

    New tables for Scheman and Seeds

---
+++ db/seeds/02_items.sql
@@ -0,0 +1,14 @@
+-- Widgets table seeds here (Example)
+INSERT INTO categories (category_type)
+VALUES ('To watch');
+('To eat'),
+('To read'),
+('To buy');
+
+
+INSERT INTO items (user_id, category_id, title) VALUES (1, 2, 'Pizza');
+INSERT INTO items (user_id, category_id, title) VALUES (2, 3, 'Shoes');
+INSERT INTO items (user_id, category_id, title) VALUES (2, 4, 'Bread');
+INSERT INTO items (user_id, category_id, title) VALUES (1, 1, 'Avengers');
+INSERT INTO items (user_id, category_id, title) VALUES (2, 2, 'Shoppers Drug Mart');
+INSERT INTO items (user_id, category_id, title) VALUES (1, 4, 'Cologne');
