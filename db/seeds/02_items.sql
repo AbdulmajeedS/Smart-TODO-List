399ee611e44b00be1c0b96404e34c8a6521c2025
alparslanustaa <alparslanustaa@gmail.com>
2022-02-17 19:28:24 +0000

    new list add

--- db/seeds/02_items.sql
+++ db/seeds/02_items.sql
@@ -5,9 +5,22 @@ INSERT INTO categories (category_type) VALUES ('To read');
 INSERT INTO categories (category_type) VALUES ('To buy');
 INSERT INTO categories (category_type) VALUES ('Un-Catogorise');
 
+
+INSERT INTO items (user_id, category_id, title) VALUES ('1', '1', 'Cloud 9');
+INSERT INTO items (user_id, category_id, title) VALUES ('1', '1', 'Fox News');
+INSERT INTO items (user_id, category_id, title) VALUES ('1', '1', 'Frozen');
 INSERT INTO items (user_id, category_id, title) VALUES ('1', '2', 'Pizza');
-INSERT INTO items (user_id, category_id, title) VALUES ('2', '4', 'Shoes');
-INSERT INTO items (user_id, category_id, title) VALUES ('2', '4', 'Bread');
+INSERT INTO items (user_id, category_id, title) VALUES ('1', '4', 'Shoes');
+INSERT INTO items (user_id, category_id, title) VALUES ('1', '4', 'Bread');
 INSERT INTO items (user_id, category_id, title) VALUES ('1', '1', 'Avengers');
-INSERT INTO items (user_id, category_id, title) VALUES ('2', '4', 'Shoppers Drug Mart');
+INSERT INTO items (user_id, category_id, title) VALUES ('1', '4', 'Mask');
 INSERT INTO items (user_id, category_id, title) VALUES ('1', '2', 'Cologne');
+INSERT INTO items (user_id, category_id, title) VALUES ('1', '2', 'Oranges');
+INSERT INTO items (user_id, category_id, title) VALUES ('1', '4', 'Dress');
+INSERT INTO items (user_id, category_id, title) VALUES ('1', '4', 'Cheese');
+INSERT INTO items (user_id, category_id, title) VALUES ('1', '2', 'News');
+INSERT INTO items (user_id, category_id, title) VALUES ('1', '4', 'Macbook');
+INSERT INTO items (user_id, category_id, title) VALUES ('1', '2', 'Cologne');
+INSERT INTO items (user_id, category_id, title) VALUES ('1', '3', 'NewsPaper');
+INSERT INTO items (user_id, category_id, title) VALUES ('1', '3', 'Front End Developer Life');
+INSERT INTO items (user_id, category_id, title) VALUES ('1', '3', 'Porject requirements');
