225644dfc0a35ed1f0ab598559db5483d68a3139
alparslanustaa <alparslanustaa@gmail.com>
2022-02-17 02:07:08 +0000

    application for adding lists document refactor

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,31 +1,31 @@
 // Client facing scripts here
-$(document).ready(function(){
+$(document).ready(function () {
 
-  $("ul").on("click", "li", function(){
-  $(this).toggleClass("completed");
+  $("ul").on("click", "li", function () {
+    $(this).toggleClass("completed");
   });
 
 
-  $("ul").on("click", "span", function(event){
-  $(this).parent().fadeOut(500,function(){
-  $(this).remove();
-  });
-  event.stopPropagation();
-  });
-
-  $("input[type='text']").keypress(function(event){
-  if(event.which === 13){
-
-  var todoText = $(this).val();
-  $(this).val("");
-
-  $("ul").append("<li><span><i class='fa fa-trash'></i></span> " + todoText + "</li>")
-  }
+  $("ul").on("click", "span", function (event) {
+    $(this).parent().fadeOut(500, function () {
+      $(this).remove();
+    });
+    event.stopPropagation();
   });
 
-  $(".fa-plus").click(function(){
-  $("input[type='text']").fadeToggle();
+  $("input[type='text']").keypress(function (event) {
+    if (event.which === 13) {
+      event.preventDefault();
+      var todoText = $(this).val();
+      $(this).val("");
+
+      $("ul").append("<li><span><i class='fa fa-trash'></i></span> " + todoText + "</li>")
+    }
+  });
+
+  $(".fa-plus").click(function () {
+    $("input[type='text']").fadeToggle();
   });
 
 
-  });
+});
